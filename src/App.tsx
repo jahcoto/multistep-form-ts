2ac046f172e4cd8950f42723192ebd3c0da5c5ef
jahcoto <jahcoto@gmail.com>
2022-11-25 00:41:19 -0600

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import useMultistepForm from "./useMultistepForm";
 function App() {
-  return <div className="App"></div>;
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([<div>ONE</div>, <div>TWO</div>]);
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        background: "white",
+        border: "1px solid black",
+        padding: "2rem",
+        margin: "1rem",
+        borderRadius: ".5rem",
+        fontFamily: "Arial",
+      }}
+    >
+      <form>
+        <div
+          style={{
+            position: "absolute",
+            top: ".5rem",
+            right: ".5rem",
+          }}
+        >
+          {currentStepIndex + 1}/ {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            gap: "5rem",
+            justifyContent: "flex-end",
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="button" onClick={next}>
+            {isLastStep ? "Finish" : "Next"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
--- src/useMultistepForm.tsx
+++ src/useMultistepForm.tsx
@@ -1,16 +1,41 @@
-import React, { ReactElement } from 'react';
-import { useState } from 'react';
+import React, { ReactElement } from "react";
+import { useState } from "react";
 
 const useMultistepForm = (steps: ReactElement[]) => {
   const [currentStepIndex, setCurrrentStepIndex] = useState(0);
 
-  function next() {}
+  function next() {
+    setCurrrentStepIndex((i) => {
+      if (i <= 0) {
+        return i;
+      }
+      return i - 1;
+    });
+  }
 
-  function back() {}
+  function back() {
+    setCurrrentStepIndex((i) => {
+      if (i >= steps.length - 1) {
+        return i;
+      }
+      return i + 1;
+    });
+  }
 
-  function goTo(index: number) {}
+  function goTo(index: number) {
+    setCurrrentStepIndex(index);
+  }
 
-  return { currentStepIndex, step: steps[currentStepIndex] };
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    steps,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    goTo,
+    next,
+    back,
+  };
 };
 
 export default useMultistepForm;
